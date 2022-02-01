b9a39d04e8205137c86b996dddf205ba417d0070
dschom <dschomburg@mozilla.com>
2022-02-01 09:10:26 -0800

    Add spinner so we know what's happening

--- _scripts/check-mysql.sh
+++ _scripts/check-mysql.sh
@@ -5,9 +5,11 @@ HOST=${2:-localhost}
 RETRY=60
 for i in $(eval echo "{1..$RETRY}"); do
   if echo PING | nc "$HOST" "$PORT" | grep -q 'mysql'; then
+    echo "mysql took $SECONDS seconds"
     exit 0
   else
     if [ "$i" -lt $RETRY ]; then
+      echo -ne "\rretrying mysql [${spin:$((i%4)):1}] ($i of $RETRY)\r"
       sleep 1
     fi
   fi
--- _scripts/check-url.sh
+++ _scripts/check-url.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -e
 
+spin='-\|/'
+
 RETRY=120
 for i in $(eval echo "{1..$RETRY}"); do
   if [ "$(curl -s -o /dev/null --silent -w "%{http_code}"  http://$1)" == "${2:-200}" ]; then
-    echo "$1 took $SECONDS seconds"
+    echo "$1 took $SECONDS seconds             "
     exit 0
   else
     if [ "$i" -lt $RETRY ]; then
+      echo -ne "\rretrying $1 [${spin:$((i%4)):1}] ($i of $RETRY)\r"
       sleep 1
     fi
   fi
