1a9a456a5d3173f709aa5c70b64cf25596173c2b
Lisa Chan <lchan@mozilla.com>
2022-04-25 17:11:33 -0400

    Fix ts files

--- packages/fxa-auth-server/lib/routes/subscriptions/google.ts
+++ packages/fxa-auth-server/lib/routes/subscriptions/google.ts
@@ -14,7 +14,9 @@ import { SkuType } from '../../payments/iap/google-play/types/purchases';
 import { IAPConfig } from '../../payments/iap/iap-config';
 import { AuthLogger, AuthRequest } from '../../types';
 import { handleAuthScoped } from './utils';
-import SUBSCRIPTIONS_DOCS from '../../../docs/swagger/subscriptions-api';
+
+const SUBSCRIPTIONS_DOCS =
+  require('../../../docs/swagger/subscriptions-api').default;
 
 export class GoogleIapHandler {
   private log: AuthLogger;
